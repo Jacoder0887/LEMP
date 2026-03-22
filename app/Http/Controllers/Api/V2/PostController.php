fe337d232852e997a66ead027346e874fe118719
Jacoder <battletupaz@gmail.com>
2026-03-22 20:29:41 +0800

    Request validate

--- app/Http/Controllers/Api/V2/PostController.php
+++ app/Http/Controllers/Api/V2/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V2;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Str;
@@ -20,12 +21,9 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StorePostRequest $request)
     {
-        $data = $request->validate([
-            'title'=> 'required|string|min:2',
-            'body'=> 'required|string|min:2',
-        ]);
+        $data = $request->validated();
         $data['author_id'] = 3;
         $data['body'] = Str::random();
         $post = Post::create($data);
@@ -53,12 +51,9 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Post $post)
+    public function update(StorePostRequest $request, Post $post)
     {
-        $data = $request->validate([
-            'title'=> 'required|string|min:2',
-            'body'=> 'required|string|min:2',
-        ]);
+        $data = $request->validatd();
 
         $post->update($data);
 
