439334c0e5024f6273cc564d0052b28f11fc7495
Jacoder <battletupaz@gmail.com>
2026-03-29 17:38:45 +0800

    add breeze for authentication

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,8 @@
 <?php
 
-
-use App\Http\Controllers\Api\V1\PostController as PostControllerV1;
-use App\Http\Controllers\Api\V2\PostController as PostControllerV2;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get('/hello', function(){
-    return ["message" => "YOW!"];
-});
-
-Route::prefix("v1")->group(function () {
-    Route::apiResource("post", PostControllerV1::class);
-});
-
-Route::prefix("v2")->group(function () {
-    Route::apiResource("post", PostControllerV2::class);
 });
